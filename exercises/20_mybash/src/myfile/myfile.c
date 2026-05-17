4ddf76fede5b7fe3cdaafdfae2a6a139401303ab
Ganyuan Chang <56210089+serf-huik@users.noreply.github.com>
2026-05-17 16:27:37 +0800

    Change exercise 20.

--- exercises/20_mybash/src/myfile/myfile.c
+++ exercises/20_mybash/src/myfile/myfile.c
@@ -2,30 +2,37 @@
 
 void print_elf_type(uint16_t e_type) {
     const char *type_str;
+    const char *enum_str;
     switch (e_type) {
         case ET_NONE:
             type_str = "Unknown";
+            enum_str = "ET_NONE";
             break;
         case ET_REL:
             type_str = "Relocatable";
+            enum_str = "ET_REL";
             break;
         case ET_EXEC:
             type_str = "Executable";
+            enum_str = "ET_EXEC";
             break;
         case ET_DYN:
             type_str = "Shared Object/PIE";
+            enum_str = "ET_DYN";
             break;
         case ET_CORE:
             type_str = "Unknown";
+            enum_str = "ET_CORE";
             break;
         default:
             type_str = "Unknown";
+            enum_str = "UNKNOWN";
             break;
     }
-    printf("ELF Type: %s (0x%x)\n", type_str, e_type);
+    printf("ELF Type: %s (%s) (0x%x)\n", type_str, enum_str, e_type);
 }
 
-int __cmd_myfile(const char* filename) {
+int __cmd_myfile(const char *filename) {
     int fd;
     Elf64_Ehdr ehdr;
 
