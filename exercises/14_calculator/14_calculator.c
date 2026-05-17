c1cde909047ab1a2ecafd34b8fa4f3f5bc29dce0
Ganyuan Chang <56210089+serf-huik@users.noreply.github.com>
2026-05-17 10:15:55 +0800

    Add error message for exercise 14.

--- exercises/14_calculator/14_calculator.c
+++ exercises/14_calculator/14_calculator.c
@@ -8,6 +8,7 @@ int subtract(int a, int b) { return a - b; }
 int multiply(int a, int b) { return a * b; }
 int divide(int a, int b) { 
     if (b == 0) {
+        printf("错误：除数不能为0\n");
         return 0;
     }
     return a / b; 
